53a8cc7ba5a3e1f31a1401cd37b393e6b8082589
ehlove154 <38021579+ehlove154@users.noreply.github.com>
2025-05-09 16:43:15 +0900

    Delete javastudy directory

--- javastudy/src/main/java/my/study/animal/component/inheritance/Chicken.java
+++
@@ -1,9 +0,0 @@
-package my.study.animal.component.inheritance;
-
-import my.study.animal.component.Gender;
-
-public class Chicken extends Bird {
-    public Chicken(Gender gender) {
-        super("Chicken", gender);
-    }
-}
--- javastudy/src/main/java/my/study/animal/feature/Flyable.java
+++
@@ -1,9 +0,0 @@
-package my.study.animal.feature;
-
-public interface Flyable {
-//    void fly();
-
-    default void fly() {
-        System.out.println("fly...");
-    }
-}
